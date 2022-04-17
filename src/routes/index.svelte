c4a4005ccfd2cc5f7694d87a4b31b0feab0c6d0d
Marcques Mouton <marcquesmouton1992@gmail.com>
2022-04-17 18:23:42 +0400

    downloadable pdf

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,5 @@
 <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
+
+  <a sveltekit:prefetch href="/request-a6e9c39c-2c26-420e-9527-cfcfe2446f8e.pdf" download class="font-extrabold underline">Download</a>
---
+++ src/routes/request-[token].pdf.js
@@ -0,0 +1,52 @@
+import https from "https";
+
+import * as crypto  from "crypto";
+
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+});
+let options = {
+    method: 'GET',agent: httpsAgent,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Basic c3VwcG9ydDpjMHYxZDIwMjA='
+    },
+  };
+export async function get({params,url}){
+
+    const data = await fetch(`https://safemauritius.govmu.org/fhirservice/ServiceRequest?_revinclude=Task:based-on&identifier=${params.token}`,options)
+    const tr = await data.json()
+    if(data.ok){
+        let pax = JSON.parse(tr.entry[1].resource.description)
+         delete pax['finalized']
+       // string to be hashed
+       const str = JSON.stringify(pax)
+
+
+       // create a sha-256 hasher
+       const sha256Hasher = crypto.createHash("sha256");
+
+       // hash the string
+       // and set the output format
+       const hash = sha256Hasher.update(str).digest("hex").toUpperCase();
+       let fileOptions = {
+        method: 'GET',agent: httpsAgent,
+        headers: {
+          Authorization: `${hash}`
+        },
+      };
+        const getFile = await fetch(`https://safemauritius.govmu.org/api/summary/${params.token}`,fileOptions)
+          
+            console.log(hash)
+            console.log(url.searchParams.get('token'))
+        if(getFile.ok){       
+                return {
+                    status: await getFile.status,   headers: {        
+                      'content-type': 'text/Uint8Array'
+                  },
+                    body: await getFile.blob()
+                } }
+
+    }
+    
+}
